75d6a415de4bfb6e7ee2af8e52bb6338a2e3847d
maria paula <mp.vargast79@gmail.com>
2020-01-29 16:49:38 -0500

    update corrections

--- sketch.js
+++ sketch.js
@@ -1,11 +1,33 @@
-function preload(){
-  // put preload code here
-}
+var myPattern = 4;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
 }
 
 function draw() {
-  // put drawing code here
+  background(mouseY / 2, mouseY / 4, 255);
+
+  for (var tileY = 0; tileY <= myPattern; tileY++) {
+    for (var tileX = 0; tileX <= 400; tileX++) {
+      var posX = windowWidth / 8 * tileX;
+      var posY = windowHeight / 4 * tileY;
+
+      strokeWeight(mouseX / 40);
+      stroke(255 - mouseY / 4, 10, 255 - mouseY / 4);
+      fill(255);
+
+      rect(posX - 82, posY - 82, 20, 20);
+      rect(posX, posY, 60, 60);
+      rect(posX, posY, 20, 20);
+      rect(posX + 40, posY + 40, 40, 40);
+      rect(posX - 40, posY - 40, 40, 40);
+      rect(posX + 40, posY - 40, 40, 40);
+      rect(posX - 40, posY + 40, 40, 40);
+    }
+  }
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
 }
